7534c42e15d2d4295ebfc71a915ebc9e11678e68
countenum404 <70239127+countenum404@users.noreply.github.com>
2021-01-18 17:47:31 +0300

    New update
    Reader method now is working:
    that splits a string to a two lists, which contains numbers and operations forming stack.
    Stack with numbers contains negative nubers, and remove 'minus' operation from stack and inserts a 'plus'.
    Calculation method still don't works.

--- Calculate.py
+++ Calculate.py
@@ -1,26 +1,58 @@
-strng = '567+78*20+311'
+
+s = '567*78*20'
 
 
-class Calculate():
-    def __init__(self, string):
+class Calculate:
+
+    def __init__(self, string: str):
         self.string = string
-        self.operators = self.reader()
-    def reader(self):
+        self.operators = self._reader()
+        self.stack = list(self.operators)
+        #self.result = self.calc()
+
+    def _reader(self):
         num = ''
         stack_num = []
         stack_ops = []
-        i = 0
-        counter = 0
         for i in self.string:
             try:
                 float(i)
             except ValueError:
                 stack_ops.append(i)
-                stack_num.append(float(num))
+                if num == '':
+                    pass
+                else:
+                    stack_num.append(float(num))
                 num = ''
             else:
                 num += str(i)
         stack_num.append(float(num))
-        return (stack_num, stack_ops)
 
+        if len(stack_num) == len(stack_ops):
+            if stack_ops[0] == '-':
+                stack_ops.pop(0)
+                stack_num[0] *= -1
+        if len(stack_num) > len(stack_ops):
+            for i in range(len(stack_ops)):
+                if stack_ops[i] == '-':
+                    stack_num[i + 1] *= -1
+                    stack_ops.pop(i)
+                    stack_ops.insert(i, '+')
 
+        return stack_num, stack_ops
+
+    def calc(self):
+        ops = self.stack[1]
+        nums = self.stack[0]
+        for i in range(len(ops)):
+            if ops[i - 1] == '*':
+                nums[i - 1] = nums[i] * nums[i - 1]
+                nums.pop(i)
+                ops.pop(i-1)
+            else:
+                pass
+        return self.stack
+
+a = Calculate(s)
+
+print(a.stack)
